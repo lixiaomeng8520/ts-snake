81f42cb9a07dce6bec6e70570f20ced9e52570ee
lixiaomeng <lixiaomeng8520@163.com>
2020-09-07 18:25:22 +0800

    * test

--- src/index.ts
+++ src/index.ts
@@ -26,14 +26,60 @@ class Pos {
 }
 
 class Game {
-    
+    snake: Snake
+    food: Food
+    isDead: boolean = false
+
+    constructor(snake: Snake, food: Food) {
+        this.snake = snake
+        this.food = food
+    }
+
+    // 移动
+    move(): void {
+        
+
+        // 获取下一个位置
+        const next: Pos = this.snake.getNext()
+        
+
+        // 出界
+        if (next.x < 0 || next.x > 4 || next.y < 0 || next.y > 4) {
+            this.isDead = true
+            return
+        }
+
+        // 碰到自己
+        for(const pos in this.snake.body) {
+            if (this.snake.body[pos].x == next.x && this.snake.body[pos].y == next.y) {
+                this.isDead = true
+                return
+            }
+        }
+
+        // 头部加上
+        this.snake.body.unshift(next)
+
+        // 是否吃到食物，如果吃到，则尾巴不动，如果没吃到，则尾巴shift
+        if (next.x != this.food.body.x || next.y != this.food.body.y) {
+            this.snake.body.pop()
+        }
+
+        console.log(this.snake.body)
+    }
+
 }
 
 class Snake {
     // 方向
-    direction: Direction = Direction.Right
+    direction: Direction
     // 身体数组
-    body: Pos[] = [new Pos(1, 0), new Pos(0, 0)]
+    body: Pos[]
+
+    constructor(direction: Direction, body: Pos[]) {
+        this.direction = direction
+        this.body = body
+    }
 
     // 获取下一个位置
     getNext(): Pos {
@@ -52,30 +98,29 @@ class Snake {
         }
         return next
     }
-
-    // 移动
-    move(): void {
-        const next: Pos = this.getNext()
-        if (next.x < 0 || next.x > 4 || next.y < 0 || next.y > 4) {
-            alert('die')
-        }
-
-    }
 }
 
 class Food {
-
+    body: Pos
+    constructor(pos: Pos) {
+        this.body = pos
+    }
 }
 
-const snake: Snake = new Snake()
-console.log(snake.getNext(), snake.body)
-
-const arr: number[] = [3]
-console.log(0 in arr)
+const snake: Snake = new Snake(Direction.Right, [new Pos(1, 0), new Pos(0, 0)])
+const food: Food = new Food(new Pos(3, 1))
+const game: Game = new Game(snake, food)
 
 document.addEventListener('keyup', (ev: KeyboardEvent) => {
     if (ev.keyCode in Direction) {
-        
+        if (Math.abs(ev.keyCode - snake.direction) % 2 == 1) {
+            game.snake.direction = ev.keyCode
+        }
     }
-    snake.direction = ev.keyCode
 })
+
+const interval: number = setTimeout(() => {
+    
+    game.move()
+    
+}, 1000)
